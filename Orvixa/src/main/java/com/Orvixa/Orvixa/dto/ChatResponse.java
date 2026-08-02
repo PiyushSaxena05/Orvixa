cce0122b5fb51da4b00c42b676190708e505cbcd
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-02 16:26:48 +0530

    Create  ChatResponse.java

---
+++ Orvixa/src/main/java/com/Orvixa/Orvixa/dto/ChatResponse.java
@@ -0,0 +1,14 @@
+package com.Orvixa.Orvixa.dto;
+
+public class ChatResponse {
+
+    private String answer;
+
+    public ChatResponse(String answer) {
+        this.answer = answer;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
