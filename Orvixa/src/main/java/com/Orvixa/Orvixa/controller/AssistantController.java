d19fbeea3091db8e487dddb7d63116d6fb53038d
Piyush Saxena <piyushsaxena168@gmail.com>
2026-08-02 16:30:52 +0530

    Create AssistantController.java

---
+++ Orvixa/src/main/java/com/Orvixa/Orvixa/controller/AssistantController.java
@@ -0,0 +1,27 @@
+package com.Orvixa.Orvixa.controller;
+
+import com.Orvixa.Orvixa.dto.ChatRequest;
+import com.Orvixa.Orvixa.dto.ChatResponse;
+import com.Orvixa.Orvixa.service.AssistantService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/assistant")
+public class AssistantController {
+
+    private final AssistantService assistantService;
+
+    public AssistantController(AssistantService assistantService) {
+        this.assistantService = assistantService;
+    }
+
+    @PostMapping("/ask")
+    public ResponseEntity<ChatResponse> ask(@RequestBody ChatRequest request) {
+        String answer = assistantService.answerQuestion(request.getUserId(), request.getQuestion());
+        return ResponseEntity.ok(new ChatResponse(answer));
+    }
+}
